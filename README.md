e4f888ba70bc10a8511d3ecc14b19999bb76e00c
Ioana Bac <baacioana@gmail.com>
2018-06-19 15:38:13 +0300

    Added responsive css styles

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 To start the application, first clone this repository.
-Install 'google-map-react' package using npm install.
 Run npm start.
--- src/Map.js
+++ src/Map.js
@@ -9,7 +9,7 @@ var google=window.google,
 function populateInfoWindow (marker, infoWindow) {
 	if (infoWindow.marker !== marker) {
 		infoWindow.marker = marker;
-		infoWindow.setContent('<div>'+marker.title+'</div>');
+		infoWindow.setContent('<div>'+marker.title+'</div>'+'<div>'+marker.address+'</div>');
 		infoWindow.open(map,marker);
 
 		infoWindow.addListener('closeclick', function() {
@@ -19,13 +19,13 @@ function populateInfoWindow (marker, infoWindow) {
 }
 
 class Map extends Component {
-	state={
+	state= {
 		locations:[
-			{title:'Campus Universitar', location:{lat:47.0447631, lng:21.9181616}}, 
-			{title:'Zoo', location:{lat:47.0506082, lng:21.9203707}}, 
-			{title:'Primaria Oradea', location:{lat:47.0559162, lng:21.9274467}}, 
-			{title:'Biblioteca Judeteana', location:{lat:47.0489117, lng:21.9216187}}, 
-			{title:'Parcul Balcescu' , location:{lat:47.0510173, lng:21.9235386}} 
+			{title:'Campus Universitar', address:'Oradea', location:{lat:47.0447631, lng:21.9181616}}, 
+			{title:'Zoo', address:'Calea Matei Basarab, nr.1 ', location:{lat:47.0506082, lng:21.9203707}}, 
+			{title:'Primaria Oradea', address:'strada Primariei, nr. 1', location:{lat:47.0559162, lng:21.9274467}}, 
+			{title:'Biblioteca Judeteana', address: 'strada Armatei Romane, nr. 1', location:{lat:47.0489117, lng:21.9216187}}, 
+			{title:'Parcul Balcescu', address: 'strada Sucevei, nr. 79', location:{lat:47.0510173, lng:21.9235386}} 
 		],
 		filteredLocations:[],
 		markers:[]
@@ -47,12 +47,14 @@ class Map extends Component {
 		if(this.state.filteredLocations.length === 0){
 			this.state.locations.map((location)=> {
 				var position= location.location;
-				var title=location.title;
+				var title= location.title;
+				var address= location.address;
 				var marker= new google.maps.Marker({
-					map:map,
-					position:position,
-					title:title,
-					animation:google.maps.Animation.DROP,
+					map: map,
+					position: position,
+					title: title,
+					address: address,
+					animation: google.maps.Animation.DROP,
 					id: location.title
 				});
 				this.state.markers.push(marker);
@@ -64,12 +66,14 @@ class Map extends Component {
 		} else {
 			this.state.filteredLocations.map((location)=> {
 				var position= location.location;
-				var title=location.title;
+				var title= location.title;
+				var address= location.address;
 				var marker= new google.maps.Marker({
-					map:map,
-					position:position,
-					title:title,
-					animation:google.maps.Animation.DROP,
+					map: map,
+					position: position,
+					title: title,
+					address: address,
+					animation: google.maps.Animation.DROP,
 					id: location.title
 				});
 				this.state.markers.push(marker);
@@ -110,6 +114,16 @@ class Map extends Component {
 		});
 	}
 
+	hamburgerMenuClick (e) {
+		var menu= document.getElementById('filterMenu');
+		menu.classList.add('open');
+	}
+
+	closeHamburgerMenu () {
+		var menu= document.getElementById('filterMenu');
+		menu.classList.remove('open');
+	}
+
 
 	
 
@@ -119,36 +133,45 @@ class Map extends Component {
 		var filteredLocations=this.state.filteredLocations;
     	var allLocations=this.state.locations;
 		return (
-			<div id='filterMenu'>
-				<h2>Oradea Locations</h2>
-				<select id='searchField' onChange={(e) => 
-					this.filterLocations(e.target.value)}>
-					<option value='Biblioteca Judeteana'>Biblioteca Judeteana</option>
-					<option value='Campus Universitar'>Campus Universitar</option>
-					<option value='Parcul Balcescu'>Parcul Balcescu</option>
-					<option value='Primaria Oradea'>Primaria Oradea</option>
-					<option value='Zoo'>Zoo</option>
-				</select>
-				<button id='resetFilters' onClick={()=>this.resetFilters()}>Reset</button>
-				<ul id='listedLocations'>
-					{filteredLocations.length > 0 && (
-						<div>
-							{filteredLocations.map((location) => (
-								<li key={location.title} onClick={(e)=>this.handleClick(e.target)}>{location.title}</li>
-							))}
-						</div>
+			<div>
+				<div id='hamburgerMenu' onClick={(e)=>this.hamburgerMenuClick(e)}>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+				<div id='filterMenu'>
+					<button id='closeButton' onClick={(e)=>this.closeHamburgerMenu(e)}>X</button>
+					<h2>Oradea Locations</h2>
+					<select id='searchField' onChange={(e) => 
+						this.filterLocations(e.target.value)}>
+						<option value='Biblioteca Judeteana'>Biblioteca Judeteana</option>
+						<option value='Campus Universitar'>Campus Universitar</option>
+						<option value='Parcul Balcescu'>Parcul Balcescu</option>
+						<option value='Primaria Oradea'>Primaria Oradea</option>
+						<option value='Zoo'>Zoo</option>
+					</select>
+					<button id='resetFilters' onClick={()=>this.resetFilters()}>Reset</button>
+					<ul id='listedLocations'>
+						{filteredLocations.length > 0 && (
+							<div>
+								{filteredLocations.map((location) => (
+									<li key={location.title} onClick={(e)=>this.handleClick(e.target)}>{location.title}</li>
+								))}
+							</div>
+							
+						)}
+						{filteredLocations.length === 0 && (
+							<div>
+								{allLocations.map((location) => (
+									<li key={location.title} onClick={(e)=>this.handleClick(e.target)}>{location.title}</li>
+								))}
+							</div>
+						)}
 						
-					)}
-					{filteredLocations.length === 0 && (
-						<div>
-							{allLocations.map((location) => (
-								<li key={location.title} onClick={(e)=>this.handleClick(e.target)}>{location.title}</li>
-							))}
-						</div>
-					)}
-					
-				</ul>
-			</div>	
+					</ul>
+				</div>	
+			</div>
+			
 		);
 	}
 	
